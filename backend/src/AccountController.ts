a29cb030cd33180198f9d7e6b47b6e457d485c51
Garrett Blankenship <g.blankenship456@gmail.com>
2025-10-14 18:07:04 -0500

    Added createAccount method

--- backend/src/AccountController.ts
+++ backend/src/AccountController.ts
@@ -28,6 +28,10 @@ export class AccountController {
         return sessionToken;
     }
 
+    public async isValidSession(sessionToken: Session): Promise<boolean> {
+
+    }
+
     public async login(email: string, password: string): Promise<Session|null> {
         // Simple login function to check for existing user and matching password.
         // If authenticates, it will create a new session for the user
@@ -51,6 +55,34 @@ export class AccountController {
         } catch (error) {
             console.error("Error finding user: ", error);
             return null; // Errored out, so no session can be returned
+        } finally {
+            // Cleanup
+            await this.prisma.$disconnect();
+        }
+    }
+
+    public async createAccount(email: string, password: string, name: string): Promise<boolean> {
+        // Creates a new user object. It returns true if the user was created successfully and false
+        // if it fails. i.e. email in use.
+        const prisma = this.prisma;
+
+        try {
+            // Creates the password hash to insert to the DB
+            const salt = await bcrypt.genSalt();
+            const passwordHash = await bcrypt.hash(password, salt);
+
+            // Insert the data into the DB
+            const user: Omit<User, "id"|"sessions"> = {
+                email,
+                passwordHash,
+                name
+            };
+            await prisma.session.create({ data: user });
+
+            return true;
+        } catch (error) {
+            console.error("Error finding user: ", error);
+            return false; // Errored out, so no session can be returned
         } finally {
             // Cleanup
             await this.prisma.$disconnect();
