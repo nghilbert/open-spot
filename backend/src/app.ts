275a5d39756034bd8f9a9ff8442aeac0a20fea09
Nathan Hilbert <nathanghilbert@gmail.com>
2025-11-29 13:33:01 -0600

    Updated naming in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,13 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import createRoutes from "./routes";
-import { emailController } from "./controllers";
 
 export function createExpressApp() {
-	const app = express();
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: true }));
-	app.use(cookieParser());
-	
-	app.use("/api", createRoutes());
-	return app;
+	const expressApp = express();
+	expressApp.use(express.json());
+	expressApp.use(express.urlencoded({ extended: true }));
+	expressApp.use(cookieParser());
+
+	expressApp.use("/api", createRoutes());
+	return expressApp;
 }
