647af26a2fc281d252ab84f601953d7b1f599f77
Jeff Armour <jrarmour0691@gmail.com>
2025-11-28 18:00:19 -0600

    Connected Frontend and Backend for edit Location Button

--- backend/src/routes/location.routes.ts
+++ backend/src/routes/location.routes.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { requireAuth } from "../middleware";
+import { requireAuth, verifyAdmin } from "../middleware";
 import { locationController } from "../controllers";
 
 export default function createLocationRoutes() {
@@ -14,6 +14,47 @@ export default function createLocationRoutes() {
       res.status(500).json({ error: "Failed to get locations" });
     }
   });
+
+   router.post("/update", requireAuth, verifyAdmin, async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.body.data.modelLocationID);
+
+    const location = await locationController.getLocation(id);
+
+    if (!location) {
+      return res.status(404).json({ error: "Failed to find the location" });
+    }
+
+    
+    // calc spotAvailability
+    const total = Number(req.body.data.modelTotal);
+    const used = Number(req.body.data.modelUsed);
+    const spotAvailability = total - used;
+
+    const update = await locationController.updateLocation(
+      id,
+      Number(req.body.data.modelNumber), // house number
+      req.body.data.modelStreet,
+      req.body.data.modelCity,
+      req.body.data.modelState,
+      Number(req.body.data.modelZip),
+      req.body.data.modelName,
+      total,
+      spotAvailability,
+      req.body.data.modelPermit,
+      req.body.data.modelType
+    );
+
+    if (update) {
+      res.status(200).json({ message: "Update received", data: update });
+    } else {
+      res.status(400).json({ error: "Failed to update location" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
   return router;
 }
