8050ba3c03af033b1d4c11a6b3e221af959bf4e3
Nathan Hilbert <nathanghilbert@gmail.com>
2025-11-29 15:05:55 -0600

    formatted location routes before updating

--- backend/src/routes/location.routes.ts
+++ backend/src/routes/location.routes.ts
@@ -3,58 +3,57 @@ import { requireAuth, verifyAdmin } from "../middleware";
 import { locationController } from "../controllers";
 
 export default function createLocationRoutes() {
-  const router = Router();
+	const router = Router();
 
-  router.get("/getAll", requireAuth, async (req: Request, res: Response) => {
-    try {
-      const locations = await locationController.getAll();
-      res.status(200).json(locations);
-    } catch (error) {
-      console.error("Failed to get locations:", error);
-      res.status(500).json({ error: "Failed to get locations" });
-    }
-  });
+	router.get("/getAll", requireAuth, async (req: Request, res: Response) => {
+		try {
+			const locations = await locationController.getAll();
+			res.status(200).json(locations);
+		} catch (error) {
+			console.error("Failed to get locations:", error);
+			res.status(500).json({ error: "Failed to get locations" });
+		}
+	});
 
-   router.post("/update", requireAuth, verifyAdmin, async (req: Request, res: Response) => {
-  try {
-    const id = Number(req.body.data.modelLocationID);
+	router.post("/update", requireAuth, verifyAdmin, async (req: Request, res: Response) => {
+		try {
+			const id = Number(req.body.data.modelLocationID);
 
-    const location = await locationController.getLocation(id);
+			const location = await locationController.getLocation(id);
 
-    if (!location) {
-      return res.status(404).json({ error: "Failed to find the location" });
-    }
+			if (!location) {
+				return res.status(404).json({ error: "Failed to find the location" });
+			}
 
-    
-    // calc spotAvailability
-    const total = Number(req.body.data.modelTotal);
-    const used = Number(req.body.data.modelUsed);
-    const spotAvailability = total - used;
+			// calc spotAvailability
+			const total = Number(req.body.data.modelTotal);
+			const used = Number(req.body.data.modelUsed);
+			const spotAvailability = total - used;
 
-    const update = await locationController.updateLocation(
-      id,
-      Number(req.body.data.modelNumber), // house number
-      req.body.data.modelStreet,
-      req.body.data.modelCity,
-      req.body.data.modelState,
-      Number(req.body.data.modelZip),
-      req.body.data.modelName,
-      total,
-      spotAvailability,
-      req.body.data.modelPermit,
-      req.body.data.modelType
-    );
+			const update = await locationController.updateLocation(
+				id,
+				Number(req.body.data.modelNumber), // house number
+				req.body.data.modelStreet,
+				req.body.data.modelCity,
+				req.body.data.modelState,
+				Number(req.body.data.modelZip),
+				req.body.data.modelName,
+				total,
+				spotAvailability,
+				req.body.data.modelPermit,
+				req.body.data.modelType
+			);
 
-    if (update) {
-      res.status(200).json({ message: "Update received", data: update });
-    } else {
-      res.status(400).json({ error: "Failed to update location" });
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Server error" });
-  }
-});
+			if (update) {
+				res.status(200).json({ message: "Update received", data: update });
+			} else {
+				res.status(400).json({ error: "Failed to update location" });
+			}
+		} catch (err) {
+			console.error(err);
+			res.status(500).json({ error: "Server error" });
+		}
+	});
 
-  return router;
+	return router;
 }
