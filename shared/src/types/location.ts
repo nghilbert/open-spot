3e664ef7706c5256e471ec1adf3e4b5e739a7fb0
Nathan Hilbert <nathanghilbert@gmail.com>
2025-11-29 15:51:06 -0600

    updated shared location type

--- shared/src/types/location.ts
+++ shared/src/types/location.ts
@@ -1,14 +1,15 @@
 import type { Address } from "./address";
 
 export type LocationType = "PARKING_LOT" | "BIKE_RACK" | "NONE";
-export type Permit =  "COMMUTER" | "EMPLOYEE" | "GARAGE" | "NONE";
+export type Permit = "COMMUTER" | "EMPLOYEE" | "GARAGE" | "NONE";
 
 export type Location = {
 	id: number;
 	name: string;
 	spotCapacity: number;
 	spotAvailability: number;
-	permit: Permit
+	permit: Permit;
 	type: LocationType;
 	address: Address;
+	timeLimitSecs: number;
 };
