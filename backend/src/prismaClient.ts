c8f23ff4c3ed991b857dc9e030d4e8b9f4c94877
Garrett Blankenship <g.blankenship456@gmail.com>
2025-12-01 21:15:07 -0600

    Updated prisma

--- backend/src/prismaClient.ts
+++ backend/src/prismaClient.ts
@@ -1,2 +1,8 @@
 import { PrismaClient } from "@prisma/client";
-export const prismaClient = new PrismaClient();
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+const adapter = new PrismaBetterSqlite3({
+    url: process.env.DATABASE_URL || 'file:./prisma/schema/dev.db'
+});
+
+export const prismaClient = new PrismaClient({ adapter });
